bec21cf4adc5b7572b275f32f35ea14797ebe136
Max <max@wtf403.xyz>
2026-07-07 06:42:49 +0300

    Initial commit: PWA with interactive images

---
+++ sw.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = 'wiigos-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/passport.html',
+  '/imgs/home.png',
+  '/imgs/passp.png',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache.map(url => new Request(url, {cache: 'reload'})));
+      })
+      .catch(err => console.log('Cache install error:', err))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
+});
